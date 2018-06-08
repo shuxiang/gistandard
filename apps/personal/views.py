33824346499f8150bd6d721463634d0de30478d1
RobbieHan <robbie_han@outlook.com>
2018-06-08 09:02:38 +0800

    资产管理新增图片上传

--- apps/personal/views.py
+++ apps/personal/views.py
@@ -1,17 +1,21 @@
 import json
 import re
+import calendar
+from datetime import date, timedelta
 
 from django.shortcuts import render
 from django.views.generic.base import View
 from django.http import HttpResponse
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 from utils.mixin_utils import LoginRequiredMixin
 from rbac.models import Menu
 from system.models import SystemSetup
 from .forms import ImageUploadForm, UserUpdateForm
 from users.forms import AdminPasswdChangeForm
+from .models import WorkOrder
 
 User = get_user_model()
 
@@ -24,6 +28,17 @@ class PersonalView(LoginRequiredMixin, View):
     def get(self, request):
         ret = Menu.getMenuByRequestUrl(url=request.path_info)
         ret.update(SystemSetup.getSystemSetupLastData())
+        start_date = date.today().replace(day=1)
+        _, days_in_month = calendar.monthrange(start_date.year, start_date.month)
+        end_date = start_date + timedelta(days=days_in_month)
+       #(('0', '工单已退回'), ('1', '新建-保存'), ('2', '提交-等待审批'), ('3', '已审批-等待执行'), ('4', '已执行-等待确认'), ('5', '工单已完成'))
+        work_order = WorkOrder.objects.filter(Q(add_time__range=(start_date, end_date)), Q(proposer_id=request.user.id) | Q(receiver_id=request.user.id))
+        ret['work_order_1'] = work_order.filter(status="1").count()
+        ret['work_order_2'] = work_order.filter(status="2").count()
+        ret['work_order_3'] = work_order.filter(status="3").count()
+        ret['work_order_4'] = work_order.filter(status="4").count()
+        ret['start_date'] = start_date
+
         return render(request, 'personal/personal_index.html', ret)
 
 
